0dcb92c975e13ff1d23ac5d0898821139db01c38
Xavelyn <53061154+Xavelyn@users.noreply.github.com>
2022-05-23 10:05:13 +0200

    Add files via upload

---
+++ src/de/jprojekt/main/Main.java
@@ -0,0 +1,17 @@
+package de.jprojekt.main;
+
+import de.jprojekt.utils.Mysql;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Mysql.createDB();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
