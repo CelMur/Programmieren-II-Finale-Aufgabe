21642241e5b2e9c589df971fe3127a7b22492790
Jan-DHBW <ackermannjan06@gmail.com>
2022-05-23 10:37:12 +0200

    aa

---
+++ src/de/jprojekt/controller/BankAccount.java
@@ -0,0 +1,31 @@
+package de.jprojekt.controller;
+
+public abstract class BankAccount {
+	
+	String name;
+	long balance;
+	String Type;
+	long maxDebt;
+	
+	public abstract boolean withdraw();
+	
+	public abstract boolean deposit();
+
+	public long getMaxDebt() {
+		return maxDebt;
+	}
+
+	public void setMaxDebt(long maxDebt) {
+		this.maxDebt = maxDebt;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public long getBalance() {
+		return balance;
+	} 
+	
+	
+}
