f83a519d3f41139d94c5624920cc5617c8d31451
Luis Eckert <eckert.luis@gmx.de>
2022-05-25 22:59:24 +0200

    Update ActionTransferMoney.java

--- src/de/jprojekt/view/actions/ActionTransferMoney.java
+++ src/de/jprojekt/view/actions/ActionTransferMoney.java
@@ -8,5 +8,8 @@ public class ActionTransferMoney extends AbstractActionAdapter{
 
 	public ActionTransferMoney(JFrameAdapter frame) {
 		super(frame);
+		
+		putValue(NAME, "Geld überweisen");
+		putValue(SHORT_DESCRIPTION, "Öffnet einen Dialog zum Durchführen einer Überweisung");
 	}
 }
