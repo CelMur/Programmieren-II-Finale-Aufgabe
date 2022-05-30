b016ce634ee65edf5a5ead3a3b319aff86e5ac46
Xavelyn <blackdevthe@gmail.com>
2022-05-30 09:32:08 +0200

    remote add origin xavelyn@github.com CelMur/Programmieren-II-Finale-Aufgabe.git

--- src/de/jprojekt/data/models/BankAccount.java
+++ src/de/jprojekt/data/models/BankAccount.java
@@ -1,11 +1,15 @@
 package de.jprojekt.data.models;
 
 public abstract class BankAccount {
-	
+
+	int accountid;
 	String name;
 	long balance;
 	//String Type; 
 	long maxDebt;
+	int type;
+	int customerid;
+	int locked;
 	
 	public abstract boolean withdraw();
 	
