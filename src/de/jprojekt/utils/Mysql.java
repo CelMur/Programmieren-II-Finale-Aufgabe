979377af5786fe6b534fcf34978f0f7ff63418eb
Xavelyn <blackdevthe@gmail.com>
2022-05-30 12:02:01 +0200

    remote add origin xavelyn@github.com CelMur/Programmieren-II-Finale-Aufgabe.git

--- src/de/jprojekt/utils/Mysql.java
+++ src/de/jprojekt/utils/Mysql.java
@@ -14,7 +14,7 @@ public class Mysql {
         Connection con = DriverManager.getConnection(url, user, pass);
         Statement stm = con.createStatement();
 
-        String queryUser = "CREATE TABLE IF NOT EXISTS user (userid varchar(255), lastname varchar(255), firstname varchar(255), password varchar(255), salt int, address varchar(255), plz int, bday DATE, typ int);";
+        String queryUser = "CREATE TABLE IF NOT EXISTS user (userid varchar(255), username varchar(255), lastname varchar(255), firstname varchar(255), password varchar(255), salt int, address varchar(255), plz int, bday DATE, typ int);";
         String queryBanker = "CREATE TABLE IF NOT EXISTS banker (bankerid int);";
         String queryCustomer = "CREATE TABLE IF NOT EXISTS custumer (customerid int, bankerid int, accountid int);";
         String queryAccount = "CREATE TABLE IF NOT EXISTS account (accountid int, balance bigint, typ int, customerid int, name varchar(255), maxdebt bigint, locked int);";
@@ -35,8 +35,10 @@ public class Mysql {
         java.util.Date date = new java.util.Date();
         date.getTime();
 
-        System.out.println(createCustomer("Hans", "Peter", "Haribo&123", "J7 26", 68159, date, 1));
+        System.out.println(createCustomer("unique","Hans", "Peter", "Haribo&123", "J7 26", 68159, date, 1));
         System.out.println(getPassword("b48c7cd3-dff8-11ec-ab47-06470e5e03a1"));
+        System.out.println(usernameExists("test"));
+        System.out.println(usernameExists("unique"));
     }
     public static String getPassword(String userid) throws SQLException {
         String password = "";
@@ -50,6 +52,34 @@ public class Mysql {
         }
         ps.close();
         return password;
+    }
+
+    public static String getUUID(String username) throws SQLException{
+        String uuid = "";
+        Connection con = DriverManager.getConnection(url, user, pass);
+        String query = "SELECT userid FROM user WHERE username=?;";
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, username);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+            uuid = rs.getString(1);
+        }
+        ps.close();
+        return uuid;
+    }
+
+    public static String getUsername(String uuid) throws SQLException{
+        String username = "";
+        Connection con = DriverManager.getConnection(url, user, pass);
+        String query = "SELECT username FROM user WHERE userid=?;";
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, uuid);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+            username = rs.getString(1);
+        }
+        ps.close();
+        return username;
     }
 
     public static int getSalt(String userid) throws SQLException {
@@ -66,9 +96,29 @@ public class Mysql {
         return salt;
     }
 
-    public static String createCustomer(String lastname, String firstname, String nonHashedPassword, String address, int plz, Date bday, int typ) throws SQLException{
+    public static boolean usernameExists(String username) throws SQLException {
+        boolean usernameExists;
+        String result = "";
         Connection con = DriverManager.getConnection(url, user, pass);
-        String query = "INSERT INTO user (userid, lastname, firstname, password, salt, address, plz, bday, typ) VALUES (UUID(), ?, ?, ?, ?, ?, ?, ?, ?);";
+        String query = "SELECT username FROM user WHERE username=?;";
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, username);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+            result = rs.getString(1);
+        }
+        ps.close();
+        if(result.length() == 0){
+            return false;
+        }else {
+            return true;
+        }
+
+    }
+
+    public static String createCustomer(String username, String lastname, String firstname, String nonHashedPassword, String address, int plz, Date bday, int typ) throws SQLException{
+        Connection con = DriverManager.getConnection(url, user, pass);
+        String query = "INSERT INTO user (userid, username, lastname, firstname, password, salt, address, plz, bday, typ) VALUES (UUID(), ?, ?, ?, ?, ?, ?, ?, ?, ?);";
         PreparedStatement ps = con.prepareStatement(query);
 
         if(!Checks.isName(lastname)){
@@ -90,14 +140,15 @@ public class Mysql {
         java.sql.Date sqlDate = new java.sql.Date(bday.getTime());
         int salt = 10000000 + (int)(Math.random() * ((99999999 - 10000000) + 1));
 
-        ps.setString(1, lastname);
-        ps.setString(2, firstname);
-        ps.setString(3, Krypto.getHash(nonHashedPassword, Integer.toString(salt)));
-        ps.setInt(4, salt);
-        ps.setString(5, address);
-        ps.setInt(6, plz);
-        ps.setDate(7, sqlDate);
-        ps.setInt(8, typ);
+        ps.setString(1, username);
+        ps.setString(2, lastname);
+        ps.setString(3, firstname);
+        ps.setString(4, Krypto.getHash(nonHashedPassword, Integer.toString(salt)));
+        ps.setInt(5, salt);
+        ps.setString(6, address);
+        ps.setInt(7, plz);
+        ps.setDate(8, sqlDate);
+        ps.setInt(9, typ);
 
         ps.executeUpdate();
         ps.close();
