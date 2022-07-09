b199908bf636b906b524b525c7d0864bdf13e841
korren <m.woelke@hotmail.de>
2022-07-09 12:35:17 +0200

    formatting

--- src/de/jprojekt/controller/BankAccountController.java
+++ src/de/jprojekt/controller/BankAccountController.java
@@ -9,67 +9,74 @@ import de.jprojekt.utils.mysql.*;
 import de.jprojekt.data.models.*;
 import de.jprojekt.utils.BankingException;
 import java.time.LocalDate;
-import java.time.LocalDateTime; 
+import java.time.LocalDateTime;
 
 public class BankAccountController implements IBankAccountController {
 
     @Override
     public void changeMaxDebt(BankAccount b, long amount, Employee e) throws BankingException {
-        if(amount < 0 ) {
+        if (amount < 0) {
             throw new BankingException("Es ist nur ein positiver Betrag erlaubt.");
         }
-        if(b instanceof SavingAccount){
+        if (b instanceof SavingAccount) {
             throw new BankingException("Sie können den maximalen Schuldenbetrag nicht auf ein Sparbuch setzen.");
         }
-        try{
-        DBAccount.setMaxdebt(b.getName(), amount);
-        }catch(Exception ex){
+        try {
+            DBAccount.setMaxdebt(b.getName(), amount);
+        } catch (Exception ex) {
             throw new BankingException("Es ist ein Fehler aufgetreten.");
         }
         b.setMaxDebt(amount);
     }
+
     // soll ein Benutzer einen Konto erstellen können?
     @Override
     public BankAccount create(Customer user, String name, int type) throws BankingException {
         switch (type) {
             case 0:
-            try{
-                if(DBAccount.createAccount(user.getId(),name,type) == null) {
-                    throw new BankingException("Dieses Konto existiert bereits.");
-                };
-                return new DepositAccount(name, user);
-            }catch(Exception ex){
-                throw new BankingException("Es ist ein Fehler aufgetreten.");
-            }
+                try {
+                    if (DBAccount.createAccount(user.getId(), name, type) == null) {
+                        throw new BankingException("Dieses Konto existiert bereits.");
+                    }
+                    ;
+                    return new DepositAccount(name, user);
+                } catch (Exception ex) {
+                    throw new BankingException("Es ist ein Fehler aufgetreten.");
+                }
             case 1:
-            try{
-                if(DBAccount.createAccount(user.getId(),name,type) == null) {
-                    throw new BankingException("Dieses Konto existiert bereits.");
-                };
-                return new SavingAccount(name, user);
-            }catch(Exception ex){
-                throw new BankingException("Es ist ein Fehler aufgetreten.");
-            }
+                try {
+                    if (DBAccount.createAccount(user.getId(), name, type) == null) {
+                        throw new BankingException("Dieses Konto existiert bereits.");
+                    }
+                    ;
+                    return new SavingAccount(name, user);
+                } catch (Exception ex) {
+                    throw new BankingException("Es ist ein Fehler aufgetreten.");
+                }
             case 2:
-            try{
-                if(DBAccount.createAccount(user.getId(),name,type) == null) {
-                    throw new BankingException("Dieses Konto existiert bereits.");
-                };
-                return new GiroAccount(name, user);
-            }catch(Exception ex){
-                throw new BankingException("Es ist ein Fehler aufgetreten.");
-            }
+                try {
+                    if (DBAccount.createAccount(user.getId(), name, type) == null) {
+                        throw new BankingException("Dieses Konto existiert bereits.");
+                    }
+                    ;
+                    return new GiroAccount(name, user);
+                } catch (Exception ex) {
+                    throw new BankingException("Es ist ein Fehler aufgetreten.");
+                }
         }
         throw new BankingException("Es ist ein Fehler aufgetreten.");
-    }   
-    /*Erstellen eines neuen Bankkontos für einen Benutzer.
-     *@param type 0 = DepositAccount 1 = SavingAccount 2= GiroAccount
+    }
+
+    /*
+     * Erstellen eines neuen Bankkontos für einen Benutzer.
+     * 
+     * @param type 0 = DepositAccount 1 = SavingAccount 2= GiroAccount
      */
     @Override
     public BankAccount create(Customer user, String name, int type, Employee e) throws BankingException {
-        
+
         try {
-            if(DBUser.exists(user.getId()) != false) {
+            if (DBUser.exists(user.getId()) != false) {
                 throw new BankingException("Der Benutzer existiert nicht.");
             }
         } catch (SQLException e1) {
@@ -88,74 +95,71 @@ public class BankAccountController implements IBankAccountController {
     }
 
     @Override
-    public void delete(Customer c,BankAccount b) throws BankingException {
-        if(c == b.getCustomer()){
-            try{
-            if(DBAccount.deleteAccount(b.getName()) == 1){
-                c.getBankAccounts().remove(b);
-            }
-            }catch(Exception ex){
+    public void delete(Customer c, BankAccount b) throws BankingException {
+        if (c == b.getCustomer()) {
+            try {
+                if (DBAccount.deleteAccount(b.getName()) == 1) {
+                    c.getBankAccounts().remove(b);
+                }
+            } catch (Exception ex) {
                 throw new BankingException("Es ist ein Fehler aufgetreten.");
             }
-        }
-        else{
+        } else {
             throw new BankingException("Sie sind nicht berechtigt, dieses Konto zu löschen.");
         }
     }
 
     @Override
     public void depositMoney(Customer c, BankAccount b, long amount) throws BankingException {
-        if(amount < 0){
+        if (amount < 0) {
             throw new BankingException("Es ist nur ein positiver Betrag erlaubt.");
         }
         long balance = b.getBalance();
-        try{
-        if(DBAccount.setBalance(b.getName(),amount + balance) == 1){
-            balance += amount;
-            b.setBalance(balance);
-        }
-        }catch(Exception ex){
+        try {
+            if (DBAccount.setBalance(b.getName(), amount + balance) == 1) {
+                balance += amount;
+                b.setBalance(balance);
+            }
+        } catch (Exception ex) {
             throw new BankingException("Es ist ein Fehler aufgetreten.");
         }
     }
 
     @Override
     public void dissolve(BankAccount b) throws BankingException {
-        if(b.getBalance() != 0){
+        if (b.getBalance() != 0) {
             throw new BankingException("Das Konto hat noch einen Restbetrag.");
         }
-        try{
-            if(DBAccount.deleteAccount(b.getName()) == 1){
+        try {
+            if (DBAccount.deleteAccount(b.getName()) == 1) {
                 b.getCustomer().getBankAccounts().remove(b);
             }
             b = null;
-        }catch(Exception ex){
+        } catch (Exception ex) {
             throw new BankingException("Es ist ein Fehler aufgetreten.");
         }
-        
+
     }
 
     @Override
     public void enable(BankAccount b) throws BankingException {
-        try{
-            if(DBAccount.setLocked(b.getName(),0) == 1){
+        try {
+            if (DBAccount.setLocked(b.getName(), 0) == 1) {
                 b.setLocked(true);
             }
-        }
-        catch(Exception ex){
+        } catch (Exception ex) {
             throw new BankingException("Es ist ein Fehler aufgetreten.");
         }
-        
+
     }
 
     @Override
     public void lock(BankAccount b) throws BankingException {
-        try{
-            if(DBAccount.setLocked(b.getName(),1) == 1){
+        try {
+            if (DBAccount.setLocked(b.getName(), 1) == 1) {
                 b.setLocked(false);
             }
-        }
-        catch(Exception ex){
+        } catch (Exception ex) {
             throw new BankingException("Es ist ein Fehler aufgetreten.");
         }
     }
@@ -164,55 +168,54 @@ public class BankAccountController implements IBankAccountController {
     public void transferMoney(BankAccount src, BankAccount target, long amount) throws BankingException {
         long srcBalance = src.getBalance();
         long targetBalance = target.getBalance();
-        if(srcBalance + src.getMaxDebt() < amount){
+        if (srcBalance + src.getMaxDebt() < amount) {
             throw new BankingException("Sie haben nicht genug Geld.");
         }
-        if(amount < 0){
+        if (amount < 0) {
             throw new BankingException("Es ist nur ein positiver Betrag erlaubt.");
         }
-        try{
-        if(DBAccount.setBalance(src.getName(),srcBalance - amount) == 1 && DBAccount.setBalance(target.getName(),targetBalance + amount) == 1){
-            srcBalance -= amount;
-            targetBalance += amount;
-            src.setBalance(srcBalance);
-            target.setBalance(targetBalance);
-            LocalDate now = LocalDateTime.now().toLocalDate();
-            java.sql.Date date = java.sql.Date.valueOf(now);
-            DBTransactions.createTransaction(src.getName(), target.getName(), amount, date);
+        try {
+            if (DBAccount.setBalance(src.getName(), srcBalance - amount) == 1
+                    && DBAccount.setBalance(target.getName(), targetBalance + amount) == 1) {
+                srcBalance -= amount;
+                targetBalance += amount;
+                src.setBalance(srcBalance);
+                target.setBalance(targetBalance);
+                LocalDate now = LocalDateTime.now().toLocalDate();
+                java.sql.Date date = java.sql.Date.valueOf(now);
+                DBTransactions.createTransaction(src.getName(), target.getName(), amount, date);
+            }
+        } catch (Exception ex) {
+            throw new BankingException("Es ist ein Fehler aufgetreten.");
         }
-    }catch(Exception ex){
-        throw new BankingException("Es ist ein Fehler aufgetreten.");
-    }
-        
+
     }
 
     @Override
     public void unlock(BankAccount b) throws BankingException {
-        try{
-            if(DBAccount.setLocked(b.getName(),0) == 1){
+        try {
+            if (DBAccount.setLocked(b.getName(), 0) == 1) {
                 b.setLocked(true);
             }
-        }
-        catch(Exception ex){
+        } catch (Exception ex) {
             throw new BankingException("Es ist ein Fehler aufgetreten.");
         }
-        
+
     }
 
     @Override
     public void withdrawMoney(Customer c, BankAccount b, long amount) throws Exception {
-        if(c.getId() == b.getCustomer().getId()){
-            if(amount < 0){
+        if (c.getId() == b.getCustomer().getId()) {
+            if (amount < 0) {
                 throw new BankingException("Es ist nur ein positiver Betrag erlaubt.");
             }
             long balance = b.getBalance();
-            if(balance + b.getMaxDebt() < amount){
+            if (balance + b.getMaxDebt() < amount) {
                 throw new BankingException("Sie haben nicht genug Geld.");
             }
-            DBAccount.setBalance(b.getName(), balance-amount);
-            b.setBalance(balance-amount);
-        }
-        else{
+            DBAccount.setBalance(b.getName(), balance - amount);
+            b.setBalance(balance - amount);
+        } else {
             throw new BankingException("Sie sind nicht berechtigt, dieses Konto zu löschen.");
         }
     }
