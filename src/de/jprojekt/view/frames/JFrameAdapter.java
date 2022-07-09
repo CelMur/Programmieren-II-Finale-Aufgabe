7b4b48851320202c21736b4ec874f6d848e2f590
Luis Eckert <eckert.luis@gmx.de>
2022-07-09 13:11:04 +0200

    Update JFrameAdapter.java

--- src/de/jprojekt/view/frames/JFrameAdapter.java
+++ src/de/jprojekt/view/frames/JFrameAdapter.java
@@ -3,6 +3,12 @@ package de.jprojekt.view.frames;
 import javax.swing.JFrame;
 
 public abstract class JFrameAdapter extends JFrame{
+	
+	public JFrameAdapter() {}
+	
+	public JFrameAdapter(String name) {
+		super(name);
+	}
 
 	public abstract Object getData();
 }
