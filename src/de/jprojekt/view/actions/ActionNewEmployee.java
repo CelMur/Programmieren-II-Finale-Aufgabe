6e37605d8e9e2f82f4ead6a42c4ead0e4f0050e9
Luis Eckert <eckert.luis@gmx.de>
2022-07-09 17:13:57 +0200

    Update ActionNewEmployee.java

--- src/de/jprojekt/view/actions/ActionNewEmployee.java
+++ src/de/jprojekt/view/actions/ActionNewEmployee.java
@@ -3,11 +3,16 @@ package de.jprojekt.view.actions;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import de.jprojekt.controller.interfaces.IEmployeeController;
+import de.jprojekt.main.ApplicationController;
 import de.jprojekt.view.GuiMessages;
 import de.jprojekt.view.frames.JFrameAdapter;
+import de.jprojekt.view.frames.NewCustomerFrame;
+import de.jprojekt.view.frames.NewEmployeeFrame;
 
 public class ActionNewEmployee extends AbstractActionAdapter{
 
@@ -16,6 +21,22 @@ public class ActionNewEmployee extends AbstractActionAdapter{
 		
 		putValue(NAME, "Banker anlegen");
 		putValue(SHORT_DESCRIPTION, "öffnet den Dialog zum anlegen eines neuen Bankers");
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ApplicationController appController = ApplicationController.getInstance();
+		IEmployeeController controller = appController.getEmployeeController(); 
+		JFrameAdapter frame = getFrame();
+		
+		JDialog dialog = new NewEmployeeFrame(frame, controller);
+		
+		dialog.setTitle("Neuer Banker");
+		dialog.setSize(200, 400);
+		dialog.setLocationRelativeTo(null);
+		dialog.setResizable(false);
+		dialog.setVisible(true);
+		
 	}
 	
 }
