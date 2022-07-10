6ccbb6a93fa69760331db9125dbbe575af0bd6cc
Luis Eckert <eckert.luis@gmx.de>
2022-07-10 19:36:23 +0200

    Update JFrameMoneyTransfer.java

--- src/de/jprojekt/view/frames/JFrameMoneyTransfer.java
+++ src/de/jprojekt/view/frames/JFrameMoneyTransfer.java
@@ -13,6 +13,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -22,6 +23,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import de.jprojekt.controller.BankAccountController;
+import de.jprojekt.controller.interfaces.IBankAccountController;
 import de.jprojekt.data.models.BankAccount;
 import de.jprojekt.data.models.Customer;
 import de.jprojekt.main.ApplicationController;
@@ -30,109 +32,89 @@ import de.jprojekt.utils.BankingException;
 import de.jprojekt.utils.mysql.DBAccount;
 import de.jprojekt.utils.mysql.DBCustomer;
 
-public class JFrameMoneyTransfer extends JFrame{ 
-		private JTextField Name;
-		private JTextField IBAN;
-		private JTextArea Zweck;
-		private JButton btnAbsenden;
-		private JLabel BeschreibungName;
-		private JLabel BeschreibungIban;
-		private JLabel BeschreibungZweck;
-		private JLabel Beschreibungsrc;
-		JComboBox srcKonto;
-		private JRadioButton Transfer;
+public class JFrameMoneyTransfer extends JDialog{ 
+		private JTextField txtName;
+		private JTextField txtKontoID;
+		private JTextArea txtAmount;
+		private JButton btnTransfer;
+		
+		private JComboBox cboSrcKonto;
+		
+		
+		private IBankAccountController controller;
 
 		
-		public JFrameMoneyTransfer() {
-			super("Geldtransfer");
+		public JFrameMoneyTransfer(JFrameAdapter frame, IBankAccountController controller) {
+			super(frame);
+			
+			this.controller = controller;
+			setTitle("Geldüberweisung");
+			
 			setLayout(new FlowLayout());
 			setSize(400, 400);
 			String userid = ApplicationData.getInstance().getCurrentUser().getId();
 			BankAccount[] accounts;
+			
 			try{
 				Customer currUser = (Customer) ApplicationData.getInstance().getCurrentUser();
 				accounts = new BankAccount[currUser.getBankAccounts().size()];
 				accounts = currUser.getBankAccounts().toArray(accounts);
-				srcKonto = new JComboBox(new DefaultComboBoxModel(accounts));
+				cboSrcKonto = new JComboBox(new DefaultComboBoxModel(accounts));
 			}catch(Exception e){
 				JOptionPane.showMessageDialog(null, "Keine Konten vorhanden");
-				System.out.println(e + "Dantenbankfehler");
+				System.out.println(e + " Dantenbankfehler");
 			}
 
-
+			cboSrcKonto.setEditable(false);
+			JLabel lblBeschreibungsrc = new JLabel("Ursprungskonto");
+			add(lblBeschreibungsrc);
+			add(cboSrcKonto);
+	
+			JLabel lblBeschreibungName = new JLabel("txtName");
+			add (lblBeschreibungName);
+			txtName = new JTextField(15);
+			add (txtName);
 			
+			JLabel lblBeschreibungIban = new JLabel("Kontonummer");
+			add (lblBeschreibungIban);
+			txtKontoID = new JTextField(15);
+			add (txtKontoID);
 			
-			srcKonto.setEditable(false);
-			Beschreibungsrc = new JLabel("Ursprungskonto");
-			add(Beschreibungsrc);
-			add(srcKonto);
-
-
-	        JRadioButton weisungButton = new JRadioButton("Ãœberweisung");
-	        this.add(weisungButton);
-	        weisungButton.setSelected(true);
-
-
+			JLabel lblBeschreibungZweck = new JLabel("Verwendungszweck");
+			add (lblBeschreibungZweck);
+			txtAmount = new JTextArea(5,15);
+			add(txtAmount);
 			
-
-			ButtonGroup group = new ButtonGroup();
-	        group.add(weisungButton);
+			btnTransfer = new JButton("Freigeben");
+			add(btnTransfer);
 			
-			BeschreibungName = new JLabel("Name");
-			add (BeschreibungName);
-			Name = new JTextField(15);
-			add (Name);
-			
-			BeschreibungIban = new JLabel("Kontonummer");
-			add (BeschreibungIban);
-			IBAN = new JTextField(15);
-			add (IBAN);
-			
-			BeschreibungZweck = new JLabel("Verwendungszweck");
-			add (BeschreibungZweck);
-			Zweck = new JTextArea(5,15);
-			add (Zweck);
-			
-			btnAbsenden = new JButton("Freigeben");
-			add(btnAbsenden);
-			
-			UerberweisungtragHandler handler = new UerberweisungtragHandler();
-			weisungButton.addActionListener(handler);
-			btnAbsenden.addActionListener(handler);}			
+			btnTransfer.addActionListener(handler ->{
+				if(txtName.getText().isEmpty() || txtKontoID.getText().isEmpty() || txtAmount.getText().isEmpty()){
+					JOptionPane.showMessageDialog(null, "Bitte füllen Sie alle Felder aus!");
+				}
+				else{
+					try{
+						BankAccount srcKonto =(BankAccount) cboSrcKonto.getSelectedItem();
+						BankAccount targetKonto = controller.getAccountByID(txtKontoID.getText());
+						long amount = (long) Integer.parseInt(txtAmount.getText()); 
+						
+						controller.transferMoney(srcKonto, targetKonto, amount);
+						JOptionPane.showMessageDialog(null, "Überweisung erfolgreich abgeschlossen!");
+					}
+					catch(BankingException e) {
+						JOptionPane.showMessageDialog(null, e.getMessage());
+						System.out.println(e.getMessage());
+					}
+					catch(Exception e){
+						JOptionPane.showMessageDialog(null, "Überweisung fehlgeschlagen!");
+						System.out.println(e.getMessage());
+					}
+				}
+			});
+		}			
 		
-		private class UerberweisungtragHandler implements ActionListener{
-			
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				if(event.getSource() == btnAbsenden){
-					if(Name.getText().isEmpty() || IBAN.getText().isEmpty() || Zweck.getText().isEmpty()){
-						JOptionPane.showMessageDialog(null, "Bitte fÃ¼llen Sie alle Felder aus!");
-					}
-					else{
-						try{
-						BankAccount Konto =(BankAccount) srcKonto.getSelectedItem();
-						ApplicationController.getInstance().getBankAccountController().transferMoney(Konto, DBAccount.getAccount(IBAN.getText()), (long) Integer.parseInt(Zweck.getText()));
-						JOptionPane.showMessageDialog(null, "Ãœberweisung erfolgreich abgeschlossen!");
-						}
-						catch(BankingException e) {
-							JOptionPane.showMessageDialog(null, e.getMessage());
-							System.out.println(e.getMessage());
-						}
-						catch(Exception e){
-							JOptionPane.showMessageDialog(null, "Ãœberweisung fehlgeschlagen!");
-							System.out.println(e.getMessage());
-						}
-					}
-			}
-			//	if (event.getSource()==tragButton) {
-					//Schau auf Ktos des Nutzers
-					//if{Kto.equals(KTO vom User){
-						//JOptionPane.showMessageDialog(null, "Passwort geÃ¤ndert!")
-						// GELD VERSCHIEBEN
-					//else if {
-		}
+		
 	}
-		
-}			
+				
 					
 
